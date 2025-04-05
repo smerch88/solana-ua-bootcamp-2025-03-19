7b5cd4704b3a9999cfae03c3ce1229db258c6f61
smerch88 <maxarmyk@gmail.com>
2025-04-05 22:31:47 +0300

    add bs58 key formatting to wallet add

--- practice-1/generate-keypair.ts
+++ practice-1/generate-keypair.ts
@@ -1,6 +1,13 @@
 import { Keypair } from "@solana/web3.js";
+import bs58 from "bs58";
+
 const keypair = Keypair.generate();
 
 console.log(`The public key is: `, keypair.publicKey.toBase58());
-console.log(`The secret key is: `, keypair.secretKey);
+
+console.log(`The secret key is (Uint8Array): `, keypair.secretKey);
+
+const base58Key = bs58.encode(keypair.secretKey);
+console.log(`The secret key in base58 format: `, base58Key);
+
 console.log(`✅ Finished!`);
